287d5ebda883c26a5239e8e52c49bdea90288bd7
Ritika <ritika.ruhal@gmail.com>
2026-01-28 16:27:17 +0530

    28Jan,2026

--- 13Jan_2026/src/test/java/SeleniumPac/Lab12.java
+++ 13Jan_2026/src/test/java/SeleniumPac/Lab12.java
@@ -1,14 +1,11 @@
 package SeleniumPac;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.time.Duration;
 import java.util.Properties;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
 
@@ -20,76 +17,37 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Lab12 {
 
     WebDriver driver;
-    Properties prop;
     WebDriverWait wait;
     Actions actions;
+    Properties prop;
 
     @BeforeClass
     public void setUp() throws Exception {
         prop = new Properties();
-        boolean loaded = false;
+        prop.load(new FileInputStream("configure.properties"));
 
-        try (InputStream is = Thread.currentThread()
-                .getContextClassLoader()
-                .getResourceAsStream("configure.properties")) {
-            if (is != null) {
-                prop.load(is);
-                loaded = true;
-                System.out.println("Loaded properties from classpath: configuration/config.properties");
-            }
-        }
-
-        if (!loaded) {
-            String fallbackPath = System.getProperty("user.dir") + File.separator + "configure.properties";
-            File f = new File(fallbackPath);
-            if (f.exists()) {
-                try (FileInputStream fis = new FileInputStream(f)) {
-                    prop.load(fis);
-                    loaded = true;
-                    System.out.println("Loaded properties from file: " + f.getAbsolutePath());
-                }
-            }
-        }
-
-        if (!loaded) {
-            throw new IllegalStateException(
-                "Could not find properties. Place at src/test/resources/configuration/config.properties "
-              + "OR projectRoot/configure.properties");
-        }
-
-        WebDriverManager.chromedriver().clearResolutionCache().forceDownload().setup();
-
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.setAcceptInsecureCerts(true);
-        // options.addArguments("--headless=new"); // enable if needed
 
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         actions = new Actions(driver);
     }
 
     private By by(String key) {
-        String locator = prop.getProperty(key);
-        if (locator == null) throw new RuntimeException("Missing key: " + key);
-        int idx = locator.indexOf('=');
-        if (idx < 1) throw new RuntimeException("Invalid locator format for key " + key + ": " + locator);
-
-        String type = locator.substring(0, idx).trim().toLowerCase();
-        String value = locator.substring(idx + 1).trim();
-
-        switch (type) {
-            case "id": return By.id(value);
-            case "name": return By.name(value);
-            case "css": return By.cssSelector(value);
-            case "xpath": return By.xpath(value);
-            case "linktext": return By.linkText(value);
-            case "partiallinktext": return By.partialLinkText(value);
-            case "class": return By.className(value);
-            case "tag": return By.tagName(value);
-            default: throw new RuntimeException("Unsupported locator type: " + type);
+        String loc = prop.getProperty(key);
+        String[] parts = loc.split("=", 2);
+        switch (parts[0].toLowerCase()) {
+            case "id": return By.id(parts[1]);
+            case "name": return By.name(parts[1]);
+            case "css": return By.cssSelector(parts[1]);
+            case "xpath": return By.xpath(parts[1]);
+            case "linktext": return By.linkText(parts[1]);
+            case "partiallinktext": return By.partialLinkText(parts[1]);
+            default: throw new RuntimeException("Invalid locator: " + key);
         }
     }
 
@@ -97,41 +55,53 @@ public class Lab12 {
     public void lab4() {
         driver.get(prop.getProperty("base.url"));
 
-        Assert.assertTrue(
-            wait.until(ExpectedConditions.titleContains(prop.getProperty("title.contains"))),
-            "Title should contain: " + prop.getProperty("title.contains")
-        );
+        Assert.assertTrue(wait.until(
+                ExpectedConditions.titleContains(prop.getProperty("title.contains"))));
 
-        WebElement desktops = wait.until(ExpectedConditions.visibilityOfElementLocated(by("desktops.menu")));
-        actions.moveToElement(desktops).pause(Duration.ofMillis(200)).perform();
-        wait.until(ExpectedConditions.elementToBeClickable(by("desktops.showAll"))).click();
+        WebElement desktops = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(by("desktops.menu")));
 
-        wait.until(ExpectedConditions.elementToBeClickable(by("mac.link"))).click();
-        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(by("mac.heading"))).isDisplayed());
+        actions.moveToElement(desktops).pause(Duration.ofMillis(500)).perform();
 
-        WebElement sort = wait.until(ExpectedConditions.visibilityOfElementLocated(by("sort.dropdown")));
-        new Select(sort).selectByVisibleText(prop.getProperty("sort.visible.text"));
+        wait.until(ExpectedConditions.elementToBeClickable(
+                by("desktops.showAll"))).click();
 
-        WebElement addBtn = wait.until(ExpectedConditions.elementToBeClickable(by("addToCart.first")));
+        wait.until(ExpectedConditions.elementToBeClickable(
+                by("mac.link"))).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                by("mac.heading")));
+
+        Select select = new Select(wait.until(
+                ExpectedConditions.visibilityOfElementLocated(by("sort.dropdown"))));
+        select.selectByVisibleText(prop.getProperty("sort.visible.text"));
+
+        WebElement add = wait.until(
+                ExpectedConditions.elementToBeClickable(by("addToCart.first")));
         ((JavascriptExecutor) driver).executeScript(
-            "arguments[0].scrollIntoView({block:'center'}); arguments[0].click();", addBtn
-        );
+                "arguments[0].scrollIntoView({block:'center'});arguments[0].click();", add);
 
-        WebElement searchBox = wait.until(ExpectedConditions.elementToBeClickable(by("top.search.box")));
-        searchBox.clear();
-        searchBox.sendKeys(prop.getProperty("search.term"));
-        wait.until(ExpectedConditions.elementToBeClickable(by("top.search.button"))).click();
+        WebElement search = wait.until(
+                ExpectedConditions.elementToBeClickable(by("top.search.box")));
+        search.clear();
+        search.sendKeys(prop.getProperty("search.term"));
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(by("search.criteria"))).clear();
+        wait.until(ExpectedConditions.elementToBeClickable(
+                by("top.search.button"))).click();
 
-        WebElement desc = wait.until(ExpectedConditions.elementToBeClickable(by("search.in.description")));
+        WebElement criteria = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(by("search.criteria")));
+        criteria.clear();
+
+        WebElement desc = wait.until(
+                ExpectedConditions.elementToBeClickable(by("search.in.description")));
         if (!desc.isSelected()) desc.click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(by("search.submit"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(
+                by("search.submit"))).click();
     }
-
     @AfterClass
     public void tearDown() {
-        if (driver != null) driver.quit();
+        driver.quit();
     }
 }
